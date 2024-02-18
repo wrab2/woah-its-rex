f4f2de88375d02d40c589d8d902a3244b6217b21
Amber <ambwuwu@gmail.com>
2024-02-18 11:41:39 -0700

    fixed finally

--- logs.js
+++ logs.js
@@ -24,7 +24,8 @@ class secureLogs {
         let luckModifier2 = 0;
         luckModifier2 +=  (gears[18] ? 0.75 : 0) + (gears[12] ? 0.35 : 0) + (gears[10] ? 0.25 : 0);
         luckModifier1 *= gears[20] ? ((verifiedOres.getLuckBoosts()[currentPickaxe] * 0.05) >= 1 ? (verifiedOres.getLuckBoosts()[currentPickaxe] * 0.05) : 1) : 1;
-        const maxLuck = (this.#maxLuck[currentPickaxe] * luckModifier1 + luckModifier2) + 1;
+        const maxLuck = ((this.#maxLuck[currentPickaxe] + luckModifier2) * luckModifier1) + 1;
+        console.log(maxLuck);
         if ((obj.stack.includes("mine.js") || obj.stack.includes("caves.js")) && luck <= maxLuck) {
             if (mine[r][c] === undefined)
                 this.#spawnLogs.push([r, c, intended, luck, fromCave]);
