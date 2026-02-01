ec2b1b76b10f86a16eba4e97dcafec668dcaa79c
Wrab <namedj82@gmail.com>
2026-02-01 20:03:39 +0300

    added a way to disable debug by adding /?release to the link

--- logs.js
+++ logs.js
@@ -1,5 +1,5 @@
 
-const debug = (document.location.href.includes("W5EJ")) || (document.location.href.includes('http://127.0.0.1:5500/'));
+const debug = !document.location.href.includes("release") && ((document.location.href.includes("W5EJ")) || (document.location.href.includes('http://127.0.0.1:5500/')));
 const logCreated = {}
 class secureLogs {
     #spawnLogs;
