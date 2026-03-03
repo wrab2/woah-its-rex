fd4598b973de5bc6ce4555bc1414c8380092f714
Wrab <namedj82@gmail.com>
2026-03-03 12:47:17 +0000

    made jim show up visually but it's bad

--- dlc/jim.js
+++ dlc/jim.js
@@ -11,11 +11,20 @@ function spawnJim(){
 	
 	if(jim.spawnJimNOW){
 		stopMining()
-		console.log("hi I am JIM")
+		jimOpen()
 	}
 	return jim.spawnJimNOW
 }
 function tryingJim(stop=false){
 	if (stop) jim.startedTrying = -1
 	else if (jim.startedTrying === -1) jim.startedTrying = Date.now()
+}
+
+function jimOpen(){
+	//johnRefresh()
+	get("jim-window-main").style.visibility="visible"
+	get("jim-portrait").style.animationName="john-swim-up"
+	get("jim-name").style.animationName="john-name-up"
+	setTimeout(()=>playSound("Johnical"), 1000)
+	jim.opened = true
 }
