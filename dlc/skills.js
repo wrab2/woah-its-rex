43ea717346fb82fa093191e6e08ad9d12156548c
adrianinoninja <verdeswablu@gmail.com>
2026-08-28 16:31:53 +0200

    did 1 (one) part of the tree will do the others in a bit

--- dlc/skills.js
+++ dlc/skills.js
@@ -176,10 +176,9 @@ let skills = {
 new Skill({
 	id:0,
 	name: "Welcome to fumo fishing",
-	description:"Unlock the shop",
+	description:"Unlocks the shop",
 	maxLevel:1,
 	cost:{
-		"Ikuyo_Kita":1,
 		"Kaito":1,
 	},
 	position: [0,0],
@@ -187,51 +186,183 @@ new Skill({
 })
 new Skill({
 	id:1,
-	name: "test1",
-	description:"an amazing description test 2",
+	name: "fumo-ical speed",
+	description:"increases your speed by 1.5x while using a fishing rod",
 	maxLevel:1,
 	cost:{
 		"Ikuyo_Kita":1,
 		"Chen":1,
 	},
-	position: [-0.5,1],
+	position: [-1,1],
 	parents: [[0,1]],
 })
 new Skill({
 	id:2,
-	name: "test2",
-	description:"an amazing description test 3",
-	maxLevel:1,
+	name: "tier up",
+	description:"increases your tier (every fumo has a tier,  you need to be at that tier or above to find it)",
+	maxLevel:7,
 	cost:{
 		"Ikuyo_Kita":1,
 		"Chen":1,
 	},
-	position: [0.5,2],
-	parents: [[0,1]],
+	position: [-2,2],
+	parents: [[1,1]],
 })
 new Skill({
 	id:3,
-	name: "test3",
-	description:"an amazing description test 3",
+	name: "bwa",
+	description:"placeholder skill",
 	maxLevel:1,
 	cost:{
 		"Ikuyo_Kita":1,
 		"Chen":1,
 	},
-	position: [-3,-2],
-	parents: [[0,1]],
+	position: [-3,3],
+	parents: [[2,1]],
 })
 new Skill({
 	id:4,
-	name: "test4",
-	description:"an amazing description test 4",
+	name: "branching out",
+	description:"unlocks the gacha and touhou skills",
 	maxLevel:1,
 	cost:{
 		"Ikuyo_Kita":1,
 		"Chen":1,
 	},
-	position: [1.3,4],
-	parents: [[2,1]],
+	position: [-4,4],
+	parents: [[3,1]],
+})
+new Skill({
+	id:5,
+	name: "beginner bait",
+	description:"doubles stock of bait in the shop",
+	maxLevel:3,
+	cost:{
+		"Rapunzel":1,
+	
+	},
+	position: [-5,3],
+	parents: [[4,1]],
+})
+new Skill({
+	id:6,
+	name: "placeholder",
+	description:"s",
+	maxLevel:1,
+	cost:{
+		"Ikuyo_Kita":1,
+		"Chen":1,
+	},
+	position: [-3,5],
+	parents: [[4,1]],
+})
+new Skill({
+	id:7,
+	name: "forbidden knowledge",
+	description:"+5% chance to not consume bait (ADDITIVE)",
+	maxLevel:5,
+	cost:{
+		"Rapunzel":1,
+	
+	},
+	position: [-6,4],
+	parents: [[5,1]],
+})
+new Skill({
+	id:8,
+	name: "forbidden-er knowledge",
+	description:"+5% chance to not consume bait (MULTIPLICATIVE)",
+	maxLevel:15,
+	cost:{
+		"Rapunzel":1,
+	
+	},
+	position: [-7,5],
+	parents: [[7,3]],
+})
+new Skill({
+	id:9,
+	name: "forbidden-est knowledge",
+	description:"^1.01 chance to not consume bait",
+	maxLevel:3,
+	cost:{
+		"Rapunzel":1,
+	
+	},
+	position: [-8,6],
+	parents: [[8,10]],
+})
+new Skill({
+	id:10,
+	name: "bait fortune",
+	description:"+10% chance to get 1 extra bait when you find bait",
+	maxLevel:10,
+	cost:{
+		"Rapunzel":1,
+	
+	},
+	position: [-6,2],
+	parents: [[5,1]],
+})
+new Skill({
+	id:11,
+	name: "bait theivery",
+	description:"makes bait fortune also work with the shop",
+	maxLevel:3,
+	cost:{
+		"Rapunzel":1,
+	
+	},
+	position: [-7,1],
+	parents: [[10,3]],
+})
+new Skill({
+	id:12,
+	name: "scavenge",
+	description:"decreases the rarity of bait, by 10,000 each upgrade",
+	maxLevel:5,
+	cost:{
+		"Rapunzel":1,
+	
+	},
+	position: [-8,0],
+	parents: [[11,1]],
+})
+new Skill({
+	id:13,
+	name: "jims hat",
+	description:"gives jim a brand new hat, increases the number of paths active at once by 1, and unlocks more (optional) upgrades",
+	maxLevel:1,
+	cost:{
+		"Rapunzel":1,
+	
+	},
+	position: [-8,3],
+	parents: [[12,3],[9,1]],
+})
+new Skill({
+	id:14,
+	name: "dark patterns",
+	description:"the first time you buy bait from the shop, the amount you earn will be multiplied by 100, furthermore if this is at level 2 this also applies to bait found (both can be triggered daily)",
+	maxLevel:2,
+	cost:{
+		"Rapunzel":1,
+	
+	},
+	position: [-6.5,3],
+	parents: [[13,1]],
+})
+new Skill({
+	id:15,
+	name: "loyalty points",
+	description:"jim airlines promotes you to a platinum customer due to your purchases (decreases cost of bait by 10%)",
+	maxLevel:3,
+	cost:{
+		"Rapunzel":1,
+	
+	},
+	position: [-9,3],
+	parents: [[13,1]],
 })
 
 if(Math.max(...tempSkills.skillIdCheck)+1 !== tempSkills.skillIdCheck.size )alert("some skill id is missing")
